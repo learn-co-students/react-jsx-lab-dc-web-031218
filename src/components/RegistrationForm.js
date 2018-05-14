e483af94caed5d357b9b5f66021cbbb5fdd78d12
SaturdayAM <jwl62@georgetown.edu>
2018-05-14 17:40:00 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const RegistrationForm = () =>(
+	<form>
+		<input type= "text"/>
+		<input type= "password"/>
+		<button type="submit">Submit</button>
+	</form>
+)	
+
+export default RegistrationForm;
---
+++ src/components/Webpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FillerText from './FillerText'
+
+const Webpage = () =>(
+	<div>
+		<h1>The world's coolest webpage</h1>
+		<FillerText/>
+		<FillerText/>
+	</div>
+)	
+
+export default Webpage;
